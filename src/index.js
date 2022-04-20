bba7bc5d724807c35b91e642bc2c041abc65bec2
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-04-20 17:02:23 +0100

    update2

--- src/index.js
+++ src/index.js
@@ -2,31 +2,48 @@ console.log(data);
 
 
 
+
+// WRITE YOUR CODE BELOW!
+
 const dogListUl = document.querySelector('.dogs-list')
 
 
 for (let i = 0; i < data.length; i++) {
-    console.log("Dog is", data[i])
-    const dog = data[i] // js object of current dog
-    console.log(dog)
+    const currentDog = data[i] // js object of current dog
+    console.log("Dog is", currentDog.name)
+    // console.log(dog)
 
     const dogListItem =  document.createElement('li')
-  // apply a style or a css class
-    dogListItem.setAttribute('class','dogs-list__button dogs-list__button--add')
-    dogListItem.innerText = dog.name
+    // apply a class to the listItem
+    // dogListItem.setAttribute('class','dogs-list__button dogs-list__button--add')
+    dogListItem.setAttribute('class','dogs-list__button')
+    dogListItem.innerText = currentDog.name
+    // add the dogListItem to the Ul
     dogListUl.append(dogListItem)
+
+
+    dogListItem.addEventListener('click', function (event) {
+        console.log("Dog clicked:", currentDog)
+        event.preventDefault()
+
+        const dogName = currentDog.name
+        if (dogName !== ) {
+            false
+        }
+        // when diff button clicked remove currentDog displayed
+    })
+
+    function NewCard (data) {
+        const mainCard = document.querySelector('.main__dog-section')
+    }
 }
 
+// remove existing section
+// create card element, h2 element
+// newCard section into the html 
 
 
 
-const dogList = document.querySelector(".dogs-list")
+// const dogList = document.querySelector(".dogs-list")
 // const dogListButton = document.querySelector(".dogs-list__button dogs-list__button--add")
 
-
-dogList.addEventListener('dogs-list__button dogs-list__button--add', function () {
-      
-
-})
-
-// WRITE YOUR CODE BELOW!
