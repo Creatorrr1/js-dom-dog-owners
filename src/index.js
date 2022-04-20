924811d2844afa6c95da9353fdf81632c024384f
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-04-20 17:55:16 +0100

    update 3

--- src/index.js
+++ src/index.js
@@ -26,24 +26,55 @@ for (let i = 0; i < data.length; i++) {
         console.log("Dog clicked:", currentDog)
         event.preventDefault()
 
-        const dogName = currentDog.name
-        if (dogName !== ) {
-            false
-        }
+        // const dogName = currentDog.name
+        const mainCard = document.querySelector('.main__dog-section')
+        mainCard.remove()
         // when diff button clicked remove currentDog displayed
+        // add new section
+        const main = document.querySelector('.main')
+        const addSection = document.createElement("section")
+        main.append(addSection)
+        // set attribute to it
+        addSection.setAttribute('class', 'main__dog-section')
+        // add h2
+        const dogH2 = document.createElement("h2")
+        // add h2 into the section
+        dogH2.innerHTML = currentDog.name
+        addSection.append(dogH2)
+        // add image
+        const mainImg = document.createElement('img')
+        mainImg.setAttribute('src', currentDog.image)
+        addSection.append(mainImg)
+        // add bio
+        const dogBio = document.createElement('h3')
+        dogBio.innerText = 'Bio'
+        addSection.append(dogBio)
+        // add paragraph
+        const dogPara = document.createElement('p')
+        dogBio.innerText = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum, minima voluptates libero cumque rerum consequatur optio aliquid sint eum maxime illo laborum omnis quo ab rem cupiditate nulla perspiciatis ipsum!'
+        addSection.append(dogPara)
+        // add div with button
+        const divButton = document.createElement('div')
+        divButton.setAttribute('class', 'main__dog-section__btn')
+        addSection.append(divButton)
+
+
     })
 
-    function NewCard (data) {
-        const mainCard = document.querySelector('.main__dog-section')
-    }
+    // function NewCard (data) {
+    //     const dogName = currentDog.name
+    //     // const card = `
+    //     // <h2>${currentDog.name}</h2>
+    //     // <img
+    //     // src="${currentDog.image}"
+    //     // alt="${currentDog.name} photo"/>
+    //     // `
+    //    mainCard.innerHTML = card;
+    // }
 }
 
 // remove existing section
 // create card element, h2 element
 // newCard section into the html 
 
-
-
-// const dogList = document.querySelector(".dogs-list")
-// const dogListButton = document.querySelector(".dogs-list__button dogs-list__button--add")
 
